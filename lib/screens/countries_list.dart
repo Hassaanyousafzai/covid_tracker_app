1b7916513b9c7be45e500e65616f95f32c8fbe12
Hassaan Yousafzai <p200088@pwr.nu.edu.pk>
2024-01-02 13:23:25 +0500

    added exceptions and internet permission

--- lib/screens/countries_list.dart
+++ lib/screens/countries_list.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:covid_tracker/components/get_api.dart';
 import 'package:covid_tracker/screens/country_details.dart';
 import 'package:flutter/material.dart';
@@ -24,34 +25,35 @@ class _CountriesScreenState extends State<CountriesScreen> {
         iconTheme: const IconThemeData(color: Colors.black),
       ),
       body: SafeArea(
-          child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: TextFormField(
-              onChanged: (value) {
-                setState(() {});
-              },
-              controller: searchController,
-              decoration: InputDecoration(
-                hintText: 'Search with country name',
-                hintStyle: const TextStyle(fontSize: 18),
-                border: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.black),
-                  borderRadius: BorderRadius.circular(20),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextFormField(
+                onChanged: (value) {
+                  setState(() {});
+                },
+                controller: searchController,
+                decoration: InputDecoration(
+                  hintText: 'Search with country name',
+                  hintStyle: const TextStyle(fontSize: 18),
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.black),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: FutureBuilder(
-              future: getAPI.getCountriesData(),
-              builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
-                if (!snapshot.hasData) {
-                  return ListView.builder(
-                      itemCount: 10,
-                      itemBuilder: (context, index) {
-                        return Shimmer.fromColors(
+            Expanded(
+              child: FutureBuilder(
+                future: getAPI.getCountriesData(),
+                builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+                  try {
+                    if (!snapshot.hasData) {
+                      return ListView.builder(
+                        itemCount: 10,
+                        itemBuilder: (context, index) {
+                          return Shimmer.fromColors(
                             baseColor: Colors.teal,
                             highlightColor: Colors.blue,
                             child: Column(
@@ -74,111 +76,98 @@ class _CountriesScreenState extends State<CountriesScreen> {
                                   ),
                                 ),
                               ],
-                            ));
-                      });
-                } else {
-                  return ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (context, index) {
-                        String countryName = snapshot.data![index]['country'];
-                        if (searchController.text.isEmpty) {
-                          return Column(
-                            children: [
-                              InkWell(
-                                onTap: () {
-                                  Navigator.push(
+                            ),
+                          );
+                        },
+                      );
+                    } else {
+                      return ListView.builder(
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (context, index) {
+                          String countryName = snapshot.data![index]['country'];
+                          if (searchController.text.isEmpty ||
+                              countryName.toLowerCase().contains(
+                                  searchController.text.toLowerCase())) {
+                            return Column(
+                              children: [
+                                InkWell(
+                                  onTap: () {
+                                    Navigator.push(
                                       context,
                                       MaterialPageRoute(
-                                          builder: (context) => CountryDetails(
-                                                image: snapshot.data![index]
-                                                    ['countryInfo']['flag'],
-                                                name: snapshot.data![index]
-                                                    ['country'],
-                                                totalCases: snapshot
-                                                    .data![index]['cases'],
-                                                totalRecovered: snapshot
-                                                    .data![index]['recovered'],
-                                                totalDeaths: snapshot
-                                                    .data![index]['deaths'],
-                                                active: snapshot.data![index]
-                                                    ['active'],
-                                                test: snapshot.data![index]
-                                                    ['tests'],
-                                                todayRecovered:
-                                                    snapshot.data![index]
-                                                        ['todayRecovered'],
-                                                population: snapshot
-                                                    .data![index]['population'],
-                                              )));
-                                },
-                                child: ListTile(
-                                  title: Text(snapshot.data![index]['country']),
-                                  subtitle: Text(snapshot.data![index]['cases']
-                                      .toString()),
-                                  leading: Image(
+                                        builder: (context) => CountryDetails(
+                                          image: snapshot.data![index]
+                                              ['countryInfo']['flag'],
+                                          name: snapshot.data![index]
+                                              ['country'],
+                                          totalCases: snapshot.data![index]
+                                              ['cases'],
+                                          totalRecovered: snapshot.data![index]
+                                              ['recovered'],
+                                          totalDeaths: snapshot.data![index]
+                                              ['deaths'],
+                                          active: snapshot.data![index]
+                                              ['active'],
+                                          test: snapshot.data![index]['tests'],
+                                          todayRecovered: snapshot.data![index]
+                                              ['todayRecovered'],
+                                          population: snapshot.data![index]
+                                              ['population'],
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                  child: ListTile(
+                                    title:
+                                        Text(snapshot.data![index]['country']),
+                                    subtitle: Text(snapshot.data![index]
+                                            ['cases']
+                                        .toString()),
+                                    leading: Image(
                                       height: 50,
                                       width: 50,
                                       image: NetworkImage(snapshot.data![index]
-                                          ['countryInfo']['flag'])),
+                                          ['countryInfo']['flag']),
+                                    ),
+                                  ),
                                 ),
-                              ),
-                            ],
-                          );
-                        } else if (countryName
-                            .toLowerCase()
-                            .contains(searchController.text.toLowerCase())) {
-                          return Column(
-                            children: [
-                              InkWell(
-                                onTap: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => CountryDetails(
-                                                image: snapshot.data![index]
-                                                    ['countryInfo']['flag'],
-                                                name: snapshot.data![index]
-                                                    ['country'],
-                                                totalCases: snapshot
-                                                    .data![index]['cases'],
-                                                totalRecovered: snapshot
-                                                    .data![index]['recovered'],
-                                                totalDeaths: snapshot
-                                                    .data![index]['deaths'],
-                                                active: snapshot.data![index]
-                                                    ['active'],
-                                                test: snapshot.data![index]
-                                                    ['tests'],
-                                                todayRecovered:
-                                                    snapshot.data![index]
-                                                        ['todayRecovered'],
-                                                population: snapshot
-                                                    .data![index]['population'],
-                                              )));
-                                },
-                                child: ListTile(
-                                  title: Text(snapshot.data![index]['country']),
-                                  subtitle: Text(snapshot.data![index]['cases']
-                                      .toString()),
-                                  leading: Image(
-                                      height: 50,
-                                      width: 50,
-                                      image: NetworkImage(snapshot.data![index]
-                                          ['countryInfo']['flag'])),
-                                ),
-                              ),
-                            ],
-                          );
-                        } else {
-                          return Container();
-                        }
-                      });
-                }
-              },
+                              ],
+                            );
+                          } else {
+                            return Container();
+                          }
+                        },
+                      );
+                    }
+                  } on SocketException catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          "Network error: $e",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
+                    return Container();
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          "An unexpected error occurred: $e",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
+                    return Container();
+                  }
+                },
+              ),
             ),
-          ),
-        ],
-      )),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:covid_tracker/components/get_api.dart';
 import 'package:covid_tracker/components/homepage_row.dart';
 import 'package:covid_tracker/models/covid_all_data.dart';
@@ -39,119 +40,158 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(15.0),
-          child: Column(
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height * .03,
-              ),
-              FutureBuilder<covidAllDataModel>(
-                future: getAllData.getCovidAllData(),
-                builder: (context, AsyncSnapshot<covidAllDataModel> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Expanded(
-                      flex: 1,
-                      child: SpinKitFadingCircle(
-                          color: Colors.black,
-                          controller: controller,
-                          size: 70),
-                    );
-                  } else if (snapshot.hasError) {
-                    return Text("${snapshot.error}");
-                  } else {
-                    return Column(
-                      children: [
-                        PieChart(
-                          animationDuration: const Duration(seconds: 3),
-                          colorList: colorList,
-                          chartType: ChartType.ring,
-                          chartRadius: MediaQuery.of(context).size.width / 1.0,
-                          dataMap: {
-                            "Total":
-                                double.parse(snapshot.data!.cases.toString()),
-                            "Recovered": double.parse(
-                                snapshot.data!.recovered.toString()),
-                            "Death":
-                                double.parse(snapshot.data!.deaths.toString()),
-                          },
-                          chartValuesOptions: const ChartValuesOptions(
-                              showChartValuesInPercentage: true),
-                        ),
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height * .05,
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.white),
-                          child: Column(
-                            children: [
-                              ReusableRow(
-                                  title: "Total",
-                                  value: snapshot.data!.cases.toString()),
-                              const Divider(
-                                color: Colors.black,
-                              ),
-                              ReusableRow(
-                                  title: "Recovered",
-                                  value: snapshot.data!.recovered.toString()),
-                              const Divider(
-                                color: Colors.black,
-                              ),
-                              ReusableRow(
-                                  title: "Death",
-                                  value: snapshot.data!.deaths.toString()),
-                              const Divider(
-                                color: Colors.black,
-                              ),
-                              ReusableRow(
-                                  title: "Active Cases",
-                                  value: snapshot.data!.active.toString()),
-                              const Divider(
-                                color: Colors.black,
-                              ),
-                              ReusableRow(
-                                  title: "Today's Cases",
-                                  value: snapshot.data!.todayCases.toString()),
-                              const Divider(
-                                height: 6,
-                              ),
-                            ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * .03,
+                ),
+                FutureBuilder<covidAllDataModel>(
+                  future: getAllData.getCovidAllData(),
+                  builder:
+                      (context, AsyncSnapshot<covidAllDataModel> snapshot) {
+                    try {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Expanded(
+                          flex: 1,
+                          child: SpinKitFadingCircle(
+                              color: Colors.black,
+                              controller: controller,
+                              size: 70),
+                        );
+                      } else if (snapshot.hasError) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(
+                              "Error: ${snapshot.error}",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            backgroundColor: Colors.red,
                           ),
-                        ),
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height * .04,
-                        ),
-                        InkWell(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => CountriesScreen()));
-                          },
-                          child: Container(
-                            height: 60,
-                            // width: 200,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Colors.green),
-                            child: const Center(
-                              child: Text(
-                                "Countries",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 25,
-                                  color: Colors.white,
+                        );
+                        return Container();
+                      } else {
+                        return Column(
+                          children: [
+                            PieChart(
+                              animationDuration: const Duration(seconds: 3),
+                              colorList: colorList,
+                              chartType: ChartType.ring,
+                              chartRadius:
+                                  MediaQuery.of(context).size.width / 1.0,
+                              dataMap: {
+                                "Total": double.parse(
+                                    snapshot.data!.cases.toString()),
+                                "Recovered": double.parse(
+                                    snapshot.data!.recovered.toString()),
+                                "Death": double.parse(
+                                    snapshot.data!.deaths.toString()),
+                              },
+                              chartValuesOptions: const ChartValuesOptions(
+                                  showChartValuesInPercentage: true),
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * .05,
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.white),
+                              child: Column(
+                                children: [
+                                  ReusableRow(
+                                      title: "Total",
+                                      value: snapshot.data!.cases.toString()),
+                                  const Divider(
+                                    color: Colors.black,
+                                  ),
+                                  ReusableRow(
+                                      title: "Recovered",
+                                      value:
+                                          snapshot.data!.recovered.toString()),
+                                  const Divider(
+                                    color: Colors.black,
+                                  ),
+                                  ReusableRow(
+                                      title: "Death",
+                                      value: snapshot.data!.deaths.toString()),
+                                  const Divider(
+                                    color: Colors.black,
+                                  ),
+                                  ReusableRow(
+                                      title: "Active Cases",
+                                      value: snapshot.data!.active.toString()),
+                                  const Divider(
+                                    color: Colors.black,
+                                  ),
+                                  ReusableRow(
+                                      title: "Today's Cases",
+                                      value:
+                                          snapshot.data!.todayCases.toString()),
+                                  const Divider(
+                                    height: 6,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * .04,
+                            ),
+                            InkWell(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const CountriesScreen()));
+                              },
+                              child: Container(
+                                height: 60,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Colors.green),
+                                child: const Center(
+                                  child: Text(
+                                    "Countries",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 25,
+                                      color: Colors.white,
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
+                          ],
+                        );
+                      }
+                    } on SocketException catch (e) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(
+                            "Network error: $e",
+                            style: TextStyle(color: Colors.white),
                           ),
+                          backgroundColor: Colors.red,
                         ),
-                      ],
-                    );
-                  }
-                },
-              ),
-            ],
+                      );
+                      return Container();
+                    } catch (e) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(
+                            "Error occurred: $e",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          backgroundColor: Colors.red,
+                        ),
+                      );
+                      return Container();
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
